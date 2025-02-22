131419a9eeb762f20b130806fe2fa57015f913f9
GMkonan <guilhemont@hotmail.com>
2025-02-22 16:20:00 -0300

    Remove git commit hash darwin version revision from config
    system.configurationRevision = self.rev or self.dirtyRev or null

--- hosts/mac/configuration.nix
+++ hosts/mac/configuration.nix
@@ -62,9 +62,6 @@ system.defaults = {
       # Enable alternative shell support in nix-darwin.
       # programs.fish.enable = true;
 
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
-
       # Used for backwards compatibility, please read the changelog before changing.
       # $ darwin-rebuild changelog
       system.stateVersion = 6;
