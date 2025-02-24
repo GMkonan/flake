656827efb397a6afd39126f9571975560a8773ce
GMkonan <guilhemont@hotmail.com>
2025-02-24 08:11:08 -0300

    Add efi support

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -16,6 +16,7 @@
   boot.loader.systemd-boot.enable = false;
   boot.loader.grub.enable = true;
   boot.loader.grub.useOSProber = true;
+  boot.loader.grub.efiSupport = true;
   boot.loader.grub.device = "nodev";
   # boot.loader.grub.theme
 
