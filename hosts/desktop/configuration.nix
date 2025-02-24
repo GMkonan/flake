2a3cfd8246e2c649356167b5fcde84f608e00b3f
GMkonan <guilhemont@hotmail.com>
2025-02-23 23:52:50 -0300

    Enable docker virtualisation (run docker daemon)

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -107,6 +107,8 @@ docker
 wl-clipboard
 ];
 
+virtualisation.docker.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
