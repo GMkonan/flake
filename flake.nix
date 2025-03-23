36f51dc411dc7bf17ba213a82e3c44d7946ec512
GMkonan <guilhemont@hotmail.com>
2025-03-22 23:24:13 -0300

    track nix-homebrew on main
    Fix brew bundle problem https://github.com/zhaofengli/nix-homebrew/issues/70

--- flake.nix
+++ flake.nix
@@ -5,7 +5,8 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin/master";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+    # nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew/main";
   };
 
   outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew }:
