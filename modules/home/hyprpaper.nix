4f8f4b57dc9ecd2b3967a8c10c2f975031ad0345
GMkonan <guilhemont@hotmail.com>
2025-04-11 22:46:47 -0300

    Add wallpapers locally

--- modules/home/hyprpaper.nix
+++ modules/home/hyprpaper.nix
@@ -1,18 +1,80 @@
-{ config, pkgs, ... }:
 {
+  pkgs,
+  lib,
+  ...
+}:
+with lib;
+with builtins; let
+  theme = {
+    name = "catppuccin-mocha"; 
+    wallpapers = ../../wallpapers;
+  };
+  themedWallpaper = wallpaper:
+    pkgs.stdenv.mkDerivation rec {
+      name = "${theme.name}-${baseNameOf wallpaper}";
+      nativeBuildInputs = [pkgs.lutgen];
+      phases = ["buildPhase" "installPhase"];
+      buildPhase = ''
+        cp ${wallpaper} ./${name}
+        lutgen apply -p ${theme.name} ${name} -o themed
+      '';
+      installPhase = ''
+        cp themed/${name} $out
+      '';
+    };
+  wallpapers = filesystem.listFilesRecursive theme.wallpapers;
+  themedWallpapers = listToAttrs (map (wallpaper: {
+      name = "${baseNameOf wallpaper}";
+      value = themedWallpaper wallpaper;
+    })
+    wallpapers);
+  wallpaperBashArray = "(\"${strings.concatStrings (strings.intersperse "\" \"" (map (wallpaper: "${wallpaper}") (attrValues themedWallpapers)))}\")";
+  wallpaperRandomizer = pkgs.writeShellScriptBin "wallpaperRandomizer" ''
+    wallpapers=${wallpaperBashArray}
+    rand=$[$RANDOM % ''${#wallpapers[@]}]
+    wallpaper=''${wallpapers[$rand]}
+    monitor=(`hyprctl monitors | grep Monitor | awk '{print $2}'`)
+    hyprctl hyprpaper unload all
+    hyprctl hyprpaper preload $wallpaper
+    for m in ''${monitor[@]}; do
+      hyprctl hyprpaper wallpaper "$m,$wallpaper"
+    done
+  '';
+in {
+  # Add lutgen and hyprpaper explicitly to home.packages
+  home.packages = with pkgs; [
+    wallpaperRandomizer
+    lutgen
+    hyprpaper
+  ];
+
   services.hyprpaper = {
     enable = true;
     settings = {
-      preload = [
-        "~/wallpapers/active/anime-girl-cold-days-ik.jpg"
-      ];
-      wallpaper = [
-        ",~/wallpapers/active/anime-girl-cold-days-ik.jpg"
-      ];
+      ipc = "on";
+      splash = false;
+      splash_offset = 2.0;
     };
   };
 
-  home.packages = with pkgs; [
-    hyprpaper
-  ];
+  systemd.user = {
+    services.wallpaperRandomizer = {
+      Install = {WantedBy = ["graphical-session.target"];};
+      Unit = {
+        Description = "Set random desktop background using hyprpaper";
+        After = ["graphical-session-pre.target"];
+        PartOf = ["graphical-session.target"];
+      };
+      Service = {
+        Type = "oneshot";
+        ExecStart = "${wallpaperRandomizer}/bin/wallpaperRandomizer";
+        IOSchedulingClass = "idle";
+      };
+    };
+    timers.wallpaperRandomizer = {
+      Unit = {Description = "Set random desktop background using hyprpaper on an interval";};
+      Timer = {OnUnitActiveSec = "1h";};
+      Install = {WantedBy = ["timers.target"];};
+    };
+  };
 }
