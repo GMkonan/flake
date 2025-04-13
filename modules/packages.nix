5576431347d6fea077ade1e6427b8c78540a6b74
GMkonan <guilhemont@hotmail.com>
2025-04-13 16:38:26 -0300

    Add biome

--- modules/packages.nix
+++ modules/packages.nix
@@ -1,47 +1,47 @@
-{ pkgs, ... }: {
+{pkgs, ...}: {
   nixpkgs.config = {
     allowUnfree = true;
-
   };
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-	neovim
-	tmux
-	obsidian
-	brave
-	slack
-	discord
-	zed-editor
-	vscode
-	bruno
-	helix
-	google-chrome
-	
-	oh-my-posh
-	zoxide
-	lsd
-	bat
-	fzf
-	fd
-	ripgrep
-	graphite-cli
-	gh
-	lazydocker
-	nodejs
-	bun
-	pnpm
-	flyctl
-	go
-	rustup
+    neovim
+    tmux
+    obsidian
+    brave
+    slack
+    discord
+    zed-editor
+    vscode
+    bruno
+    helix
+    google-chrome
+    biome
 
-	ngrok
-	silicon
+    oh-my-posh
+    zoxide
+    lsd
+    bat
+    fzf
+    fd
+    ripgrep
+    graphite-cli
+    gh
+    lazydocker
+    nodejs
+    bun
+    pnpm
+    flyctl
+    go
+    rustup
+
+    ngrok
+    silicon
   ];
 
-   fonts.packages = with pkgs; [
-     nerd-fonts.jetbrains-mono
-     # nerd-fonts.commit-mono
-   ];
+  fonts.packages = with pkgs; [
+    nerd-fonts.jetbrains-mono
+    # nerd-fonts.commit-mono
+  ];
 }
