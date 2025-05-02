770e11ddcb0b564e592d780a7797a29aa5af7f34
GMkonan <guilhemont@hotmail.com>
2025-05-02 15:21:04 -0300

    Ad modsetting nvidia

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -14,7 +14,7 @@
 
   hardware.nvidia = {
     # Modesetting is required.
-    # modesetting.enable = true;
+    modesetting.enable = true;
 
     # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
     # Enable this if you have graphical corruption issues or application crashes after waking
@@ -36,7 +36,7 @@
 
     # Enable the Nvidia settings menu,
     # accessible via `nvidia-settings`.
-    # nvidiaSettings = true;
+    nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
     # package = config.boot.kernelPackages.nvidiaPackages.latest;
