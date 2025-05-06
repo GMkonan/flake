097a312a47bf53dedc46ea2beea657a614ca90fd
GMkonan <guilhemont@hotmail.com>
2025-05-06 01:33:57 -0300

    Add vesktop overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -8,8 +8,27 @@ final: prev: {
       + ''
         echo "Wrapping slack in overlay..."
         wrapProgram $out/bin/slack \
-          --add-flags "--enable-features=WaylandLinuxDrmSyncob"
+          --add-flags "--enable-features=WaylandLinuxDrmSyncobj" \
+          --add-flags "--enable-features=UseOzonePlatform" \
+          --add-flags "-ozone-platform=wayland" \
+          --add-flags "--disable-smooth-scrolling"
+
         echo "Slack wrapping complete."
+      '';
+  });
+
+  vesktop = prev.vesktop.overrideAttrs (oldAttrs: {
+    # Ensure makeWrapper is available during the build
+    nativeBuildInputs = (oldAttrs.nativeBuildInputs or []) ++ [final.makeWrapper];
+
+    postFixup =
+      (oldAttrs.postFixup or "")
+      + ''
+        echo "Wrapping vesktop in overlay..."
+        wrapProgram $out/bin/vesktop \
+          --add-flags "--enable-features=WaylandLinuxDrmSyncobj"
+
+        echo "Vesktop wrapping complete."
       '';
   });
 }
