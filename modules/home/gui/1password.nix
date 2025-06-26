605cd6d185565caa52348f0af8ef8607afe930a3
GMkonan <guilhemont@hotmail.com>
2025-06-26 09:29:14 -0300

    Add 1password via hm

--- modules/home/gui/1password.nix
+++ modules/home/gui/1password.nix
@@ -1,9 +1,13 @@
-{...}: {
-  programs._1password.enable = true;
-  programs._1password-gui = {
-    enable = true;
-    # Certain features, including CLI integration and system authentication support,
-    # require enabling PolKit integration on some desktop environments (e.g. Plasma).
-    polkitPolicyOwners = ["konan"];
-  };
+{pkgs, ...}: {
+  home.packages = [
+    pkgs._1password
+    pkgs._1password-gui
+  ];
+  # programs._1password.enable = true;
+  # programs._1password-gui = {
+  #   enable = true;
+  #   # Certain features, including CLI integration and system authentication support,
+  #   # require enabling PolKit integration on some desktop environments (e.g. Plasma).
+  #   polkitPolicyOwners = ["konan"];
+  # };
 }
