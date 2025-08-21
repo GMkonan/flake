8d8e661cb750266d0a22e63ec534134b3f7406b2
Konan <guilhemont@hotmail.com>
2025-08-21 12:16:45 -0300

    Change pipewire stuff

--- modules/system/audio.nix
+++ modules/system/audio.nix
@@ -1,10 +1,13 @@
 {
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
+  # wpctl status
+  # wpctl set-default
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
+    jack.enable = true;
     pulse.enable = true;
   };
 }
