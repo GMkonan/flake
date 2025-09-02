e45bdf0f00fa9d31069ffd44e38a9b1c58cee720
Konan <guilhemont@hotmail.com>
2025-09-02 00:58:24 -0300

    Enable kanshi working :)

--- modules/home/wm/kanshi.nix
+++ modules/home/wm/kanshi.nix
@@ -1,41 +1,41 @@
-# {
-#   services.kanshi = {
-#     enable = true;
-#     systemdTarget = "hyprland-session.target";
-#     settings = [
-#       {
-#         profile.name = "docked";
-#         profile.outputs = [
-#           {
-#             criteria = "eDP-1";
-#             status = "disable";
-#           }
-#           {
-#             criteria = "Samsung Electric Company LU28R55 HX5W500759";
-#             # position = "1500,0";
-#             position = "0,0";
-#             scale = 2.0;
-#           }
-#           # {
-#           #   criteria = "*";
-#           #   position = "0,0";
-#           #   scale = 1.0;
-#           # }
-#         ];
-#       }
-#       {
-#         profile.name = "undocked";
-#         profile.outputs = [
-#           {
-#             status = "enable";
-#             criteria = "eDP-1";
-#             scale = 1.5;
-#           }
-#         ];
-#       }
-#     ];
-#   };
-# }
+{
+  services.kanshi = {
+    enable = true;
+    systemdTarget = "graphical-session.target";
+    settings = [
+      {
+        profile.name = "docked";
+        profile.outputs = [
+          {
+            criteria = "eDP-1";
+            status = "disable";
+          }
+          {
+            criteria = "Samsung Electric Company LU28R55 HX5W500759";
+            # position = "1500,0";
+            position = "0,0";
+            scale = 2.0;
+          }
+          # {
+          #   criteria = "*";
+          #   position = "0,0";
+          #   scale = 1.0;
+          # }
+        ];
+      }
+      {
+        profile.name = "undocked";
+        profile.outputs = [
+          {
+            status = "enable";
+            criteria = "eDP-1";
+            scale = 1.5;
+          }
+        ];
+      }
+    ];
+  };
+}
 # {
 #   services.kanshi = {
 #     enable = true;
@@ -70,38 +70,39 @@
 #     systemdTarget = "graphical-session.target";
 #   };
 # }
-{
-  services.shikane = {
-    enable = false;
-    settings = {
-      profile = [
-        {
-          name = "docked";
-          output = [
-            {
-              match = "eDP-1";
-              enable = false;
-            }
-            {
-              search = "/.*";
-              enable = true;
-              scale = 2;
-              # mode = "3840x1600@59.99300";
-            }
-          ];
-        }
-        {
-          name = "undocked";
-          output = [
-            {
-              match = "eDP-1";
-              enable = true;
-              scale = 1.5;
-              # mode = "1920x1200@60.00Hz";
-            }
-          ];
-        }
-      ];
-    };
-  };
-}
+# {
+#   services.shikane = {
+#     enable = false;
+#     settings = {
+#       profile = [
+#         {
+#           name = "docked";
+#           output = [
+#             {
+#               match = "eDP-1";
+#               enable = false;
+#             }
+#             {
+#               search = "/.*";
+#               enable = true;
+#               scale = 2;
+#               # mode = "3840x1600@59.99300";
+#             }
+#           ];
+#         }
+#         {
+#           name = "undocked";
+#           output = [
+#             {
+#               match = "eDP-1";
+#               enable = true;
+#               scale = 1.5;
+#               # mode = "1920x1200@60.00Hz";
+#             }
+#           ];
+#         }
+#       ];
+#     };
+#   };
+# }
+
