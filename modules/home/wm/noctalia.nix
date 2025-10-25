70b397826c215dc18a5df39d9020807757e774da
GMkonan <guilhemont@hotmail.com>
2025-10-25 01:09:01 -0300

    Add noctalia shell basic settings

--- modules/home/wm/noctalia.nix
+++ modules/home/wm/noctalia.nix
@@ -1,5 +1,30 @@
 {...}: {
   programs.noctalia-shell = {
     enable = true;
+
+    # https://docs.noctalia.dev/getting-started/nixos/#config-ref
+    settings = {
+      general = {
+        avatarImage = "../../../ryo.jpg";
+        lockOnSuspend = true;
+        language = "";
+      };
+      location = {
+        name = "Vitoria, ES, Brazil";
+        weatherEnabled = true;
+        use12hourFormat = false;
+        showWeekNumberInCalendar = false;
+        showCalendarEvents = true;
+      };
+      colorSchemes = {
+        predefinedScheme = "Catppuccin";
+      };
+      wallpaper = {
+        enabled = true;
+        directory = "../../../wallpapers/dark";
+        randomEnabled = true;
+        randomIntervalSec = 600;
+      };
+    };
   };
 }
