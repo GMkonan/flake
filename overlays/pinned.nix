dd787f64be31445ee8a96d99c4573c85cd2d323f
GMkonan <guilhemont@hotmail.com>
2026-03-22 15:57:48 -0300

    Remove overlay workaround for bitwarden, as it was fixed

--- overlays/pinned.nix
+++
@@ -1,6 +0,0 @@
-{nixpkgs-pinned}: final: prev: {
-  # Temporarily pin bitwarden-desktop due to electron 39.8.2 build failure
-  # See: https://github.com/NixOS/nixpkgs/issues/500399
-  # Error: 39-angle-patchdir.patch failed at electron/patches/config.json
-  bitwarden-desktop = nixpkgs-pinned.legacyPackages.${prev.system}.bitwarden-desktop;
-}
