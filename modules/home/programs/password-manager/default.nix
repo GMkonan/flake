705513db338537c9b491a465a114c989e493b80a
GMkonan <guilhemont@hotmail.com>
2026-06-24 07:40:35 -0300

    Temporarily remove bitwarden-desktop due to upstream build failure

--- modules/home/programs/password-manager/default.nix
+++ modules/home/programs/password-manager/default.nix
@@ -1,6 +1,6 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
-    bitwarden-desktop
+    # bitwarden-desktop # FIXME: temporarily removed due to upstream nixpkgs build failure
     _1password-cli
     _1password-gui
   ];
